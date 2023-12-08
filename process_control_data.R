ca0c714f99a08c73a7e9735098beb0813013b4da
EthanWaters <ethan.waters@my.jcu.edu.au>
2023-12-08 13:33:57 +1000

    check files exist before passing to other function

--- process_control_data.R
+++ process_control_data.R
@@ -38,10 +38,19 @@ main <- function(configuration_path, new_path = NULL, kml_path = NULL, leg_path 
     most_recent_new_path <- find_recent_file(configuration$metadat$input_directory$control_data, configuration$metadat$control_data_type, "csv")
     most_recent_kml_path <- find_recent_file(configuration$metadat$input_directory$spatial_data, configuration$metadat$control_data_type, "kml")
     
-    previous_report <- fromJSON(most_recent_report_path)
+    if(!is.null(most_recent_report_path)){
+      previous_report <- fromJSON(most_recent_report_path)
+    }
     
     previous_kml_path <- previous_report$inputs$kml_path
     serialised_spatial_path <- previous_report$outputs$serialized_data
+    if (!file.exists(previous_kml_path)) {
+      previous_kml_path <- NULL
+    }
+    if (!file.exists(serialised_spatial_path)) {
+      serialised_spatial_path <- NULL
+    }
+    
     
     if (is.null(new_path)) {
       new_path <- most_recent_new_path
