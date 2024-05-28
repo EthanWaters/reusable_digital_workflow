707badfb8d995087e54e2f22541786bc7592410c
EthanWaters <ethan.waters@my.jcu.edu.au>
2024-05-28 17:55:15 +1000

    remove old separation of time and dates, no longer required

--- process_control_data_research_output.R
+++ process_control_data_research_output.R
@@ -131,7 +131,7 @@ main <- function(new_path, configuration_path = NULL, kml_path = NULL, leg_path 
     tryCatch({
       if(configuration$metadata$assign_sites){
         if(configuration$metadata$control_data_type == "manta_tow"){
-          verified_data_df <- assign_nearest_site_method_c(verified_data_df, kml_path, configuration$metadata$control_data_type, calculate_site_rasters, previous_kml_path, serialised_spatial_path, raster_size=0.0005, x_closest=1, is_standardised=0, save_spatial_as_raster=0)
+          verified_data_df <- assign_nearest_site_method_c(verified_data_df, kml_path, configuration$metadata$control_data_type, previous_kml_path, serialised_spatial_path, raster_size=0.0005, x_closest=1, is_standardised=0, save_spatial_as_raster=0)
         } else {
           verified_data_df$`Nearest Site` <- site_names_to_numbers(verified_data_df$`Site Name`)
         }
